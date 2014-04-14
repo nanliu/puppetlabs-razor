4ffda4a516edb5c4e9291c831966f049d94b4486
Nan Liu <nan.liu@gmail.com>
2014-04-14 10:57:21 -0500

    Use PSON instead of JSON to reduce gem dependency.
    This removes JSON and uses puppet's internal PSON library instead.

--- lib/puppet/provider/razor.rb
+++ lib/puppet/provider/razor.rb
@@ -1,5 +1,4 @@
 require 'rest_client'
-require 'json'
 require 'tempfile'
 
 class Puppet::Provider::Razor < Puppet::Provider
@@ -9,13 +8,13 @@ class Puppet::Provider::Razor < Puppet::Provider
       response = RestClient.get(
         "http://localhost:8080/api/collections/#{type}/#{name}"
       )
-    rescue RestClient::ResourceNotFound => e
+    rescue RestClient::ResourceNotFound
       return false
     end
     if response.code == 200
-      JSON.parse(response)
+      PSON.parse(response)
     else
-      raise(Exception, "bad http code: #{response.code}:#{response.to_str}")
+      raise(Exception, "Error: http status code #{response.code}\n#{response.to_str}")
     end
   end
 
@@ -23,14 +22,14 @@ class Puppet::Provider::Razor < Puppet::Provider
     begin
       response = RestClient.post(
         "http://localhost:8080/api/commands/#{url}",
-        args.to_json,
+        args.to_pson,
         {:content_type => :json, :accept => :json}
       )
     rescue Exception => e
       fail("Rest call failed: #{e.response}")
     end
     unless response.code == 202
-      raise(Exception, "Bad response code: #{response.code}:#{response.to_str}")
+      raise(Exception, "Error: http status code #{response.code}\n#{response.to_str}")
     end
     response.code
   end
@@ -39,7 +38,7 @@ class Puppet::Provider::Razor < Puppet::Provider
   # json file that can be passed to the razor command
   def process_json_args(args = {})
     @tmp_json_file = Tempfile.new('razor_json')
-    json = args.to_json
+    json = args.to_pson
     @tmp_json_file.write(json)
     @tmp_json_file.close
     "--json #{@tmp_json_file.path}"
